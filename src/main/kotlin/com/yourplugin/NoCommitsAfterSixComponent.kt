90e426e0722fe4a2da7c952a858093caf50c127e
lukas <lukas@>
2023-10-29 23:31:00 +0100

    wip (actually compile files by renaming from java to kotlin)

---
+++ src/main/kotlin/com/yourplugin/NoCommitsAfterSixComponent.kt
@@ -0,0 +1,24 @@
+package com.yourplugin
+
+import com.intellij.openapi.components.ApplicationComponent
+import com.intellij.openapi.diagnostic.Logger
+
+class NoCommitsAfterSixComponent : ApplicationComponent {
+    companion object {
+        private val LOG: Logger = Logger.getInstance(NoCommitsAfterSixComponent::class.java)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun initComponent() {
+        LOG.info("NoCommitsAfterSix plugin has been loaded successfully")
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun disposeComponent() {
+        // add any necessary cleanup here
+    }
+
+    override fun getComponentName(): String {
+        return "NoCommitsAfterSixComponent"
+    }
+}
---
+++ src/main/kotlin/com/yourplugin/checkInHandlerFactory.kt
@@ -0,0 +1,64 @@
+package com.yourplugin;
+
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.vcs.CheckinProjectPanel;
+import com.intellij.openapi.vcs.VcsException;
+import com.intellij.openapi.vcs.changes.CommitContext;
+import com.intellij.openapi.vcs.changes.CommitExecutor;
+import com.intellij.openapi.vcs.checkin.CheckinHandler;
+import com.intellij.openapi.vcs.checkin.CheckinHandlerFactory;
+import com.intellij.openapi.vcs.ui.RefreshableOnComponent;
+import com.intellij.util.PairConsumer;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class checkInHandlerFactory extends CheckinHandlerFactory {
+    @Override
+    public @NotNull CheckinHandler createHandler(@NotNull CheckinProjectPanel panel, @NotNull CommitContext commitContext) {
+        return new CheckinHandler() {
+            @Override
+            public @Nullable RefreshableOnComponent getBeforeCheckinConfigurationPanel() {
+                return super.getBeforeCheckinConfigurationPanel();
+            }
+
+            @Override
+            public @Nullable RefreshableOnComponent getAfterCheckinConfigurationPanel(Disposable parentDisposable) {
+                return super.getAfterCheckinConfigurationPanel(parentDisposable);
+            }
+
+            @Override
+            public ReturnResult beforeCheckin(@Nullable CommitExecutor executor, PairConsumer<Object, Object> additionalDataConsumer) {
+                System.out.println("HERE checkInHandlerFactory.beforeCheckin");
+                return super.beforeCheckin(executor, additionalDataConsumer);
+            }
+
+            @Override
+            public ReturnResult beforeCheckin() {
+                System.out.println("HERE checkInHandlerFactory.beforeCheckin");
+                return super.beforeCheckin();
+            }
+
+            @Override
+            public void checkinSuccessful() {
+                super.checkinSuccessful();
+            }
+
+            @Override
+            public void checkinFailed(List<VcsException> exception) {
+                super.checkinFailed(exception);
+            }
+
+            @Override
+            public void includedChangesChanged() {
+                super.includedChangesChanged();
+            }
+
+            @Override
+            public boolean acceptExecutor(CommitExecutor executor) {
+                return super.acceptExecutor(executor);
+            }
+        };
+    }
+}
