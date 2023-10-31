dd73920422d92d7e99d1ddf8271c4de36552f773
lukas <lukas@>
2023-10-31 22:08:10 +0100

    cleanup

--- src/main/kotlin/com/yourplugin/NoCommitsAfterSixActivity.kt
+++
@@ -1,9 +0,0 @@
-//package com.yourplugin
-//
-//import com.intellij.openapi.startup.StartupActivity
-//
-//class NoCommitsAfterSixStartupActivity : StartupActivity {
-//    override fun runActivity(project: Project) {
-//        ServiceManager.getService(NoCommitsAfterSixService::class.java)
-//    }
-//}
