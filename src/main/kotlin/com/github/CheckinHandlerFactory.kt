88d529a7e8d88594582154fe99b678ec72707e0a
Lukas Baron <LukasBaron@gmail.com>
2023-11-01 18:32:32 +0100

    remove unused code from CheckinHandler

--- src/main/kotlin/com/github/CheckinHandlerFactory.kt
+++ src/main/kotlin/com/github/CheckinHandlerFactory.kt
@@ -7,25 +7,11 @@ import com.intellij.openapi.vcs.changes.CommitContext
 import com.intellij.openapi.vcs.checkin.CheckinHandler
 import com.intellij.openapi.vcs.checkin.CheckinHandlerFactory
 import org.jetbrains.annotations.NotNull
-import java.time.LocalTime
 
 class CheckinHandlerFactory : CheckinHandlerFactory() {
     companion object {
         private val LOG: Logger = Logger.getInstance(CheckinHandlerFactory::class.java)
     }
-
-// alternatives candidates for plugin name:
-//    GitGuard
-//    CommitCheckr
-//    RepoAuth
-//    SecureCommit
-//    RepoVerify
-//    GitConfigCheck
-//    CommitAuthenticator
-//    ConfigGuard
-//    RepoCommitCheck
-//    GitIdentityCheck
-
 
     @NotNull
     override fun createHandler(@NotNull checkinProjectPanel: CheckinProjectPanel, @NotNull commitContext: CommitContext): CheckinHandler {
@@ -33,21 +19,7 @@ class CheckinHandlerFactory : CheckinHandlerFactory() {
             override fun beforeCheckin(): ReturnResult {
                 val isGitRepo = Helpers.isGitProject(checkinProjectPanel.project)
 
-                val doAfterSixCheck = false
                 val doGitIdentityCheck = true
-
-                if (doAfterSixCheck) {
-                    // check if commit is attempted after 18:00
-                    val now = LocalTime.now()
-                    return if (now.isAfter(LocalTime.of(18, 0))) {
-                        LOG.info("GitIdentityChecker - Commit attempt after 18:00 - condition failed")
-                        Messages.showErrorDialog("Commits after 18:00 are not allowed.", "No Commits After Six")
-                        ReturnResult.CANCEL
-                    } else {
-                        LOG.info("GitIdentityChecker - Commit attempt before 18:00 - condition fulfilled")
-                        ReturnResult.COMMIT
-                    }
-                }
 
                 if (doGitIdentityCheck && isGitRepo) {
                     // check if a local git config files exist, its path is ${repo root}/.git/config
