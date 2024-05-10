bfd8055a2262613313d5f28c1146684d79b319c9
Lukas Baron <LukasBaron@gmail.com>
2024-05-10 22:49:34 +0200

    build with latest intellij

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-//    version.set("2023.3.1")
-    version.set("LATEST-EAP-SNAPSHOT")
+    version.set("2024.1")
+//    version.set("LATEST-EAP-SNAPSHOT")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
